e13a280443d5b24654016cd4ceb41112ac38a754
Pryesh Gohil <pryeshgohil@hotmail.com>
2024-06-30 02:00:30 +0100

    Implement Components

---
+++ app/users/[id]/photos/[photoid]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    params: {
+        id: number;
+        photoid: number;
+     }
+}
+
+const PhotoPage = ({params : {id, photoid}}: Props) => {
+  return (
+    <div>PhotoPage {id} {photoid}</div>
+  )
+}
+
+export default PhotoPage
